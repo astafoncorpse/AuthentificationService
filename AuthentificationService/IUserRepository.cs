303138fa5c15d4fc3adad240ba60087381fc2681
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-07-15 22:46:42 +0300

    Добавьте файлы проекта.

---
+++ AuthentificationService/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthentificationService
+{
+    public interface IUserRepository
+    {
+      
+        void IEnumerable<User> GetAll();
+
+         void UserGetByLogin(string login);
+    }
+
+}
---
+++ AuthentificationService/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace AuthentificationService
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        { 
+            CreateMap<User, UserViewModel>()
+            .ConstructUsing(v => new UserViewModel(v));
+        }
+
+    }
+}
